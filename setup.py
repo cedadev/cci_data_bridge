43663c5122b8a8694d3a8663d58d72126815018b
Antony Wilson <antony.wilson@stfc.ac.uk>
2023-03-20 09:03:10 +0000

    change python version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
 
     # This qualifier can be used to selectively exclude Python versions -
     # in this case early Python 2 and 3 releases
-    python_requires='>=3.7.0',
+    python_requires='>=3.6.0',
 
     # See:
     # https://www.python.org/dev/peps/pep-0301/#distutils-trove-classification
